e5246c1add2c048a9aa8dc863472b755efdff1fd
JoelMangumASC4 <joel.mangum13@gmail.com>
2017-08-16 15:36:46 -0400

    sign in page

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ $('.btn-expand-collapse').click(function(e) {
 				$('.navbar-primary').toggleClass('collapsed');
 });
 
-function storename(name) {
-    $('#profile').text(name);
+function storename() {
     
+    var name=document.getElementById("un").value;
+$('#profile').text(name);
 }
+
